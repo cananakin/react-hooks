5754bcd52d119abfa99690ef15021bec8c64bcef
Canan Akin <e.cananakin@gmail.com>
2025-09-27 12:49:01 +0100

    setup context

---
+++ src/context/product-context.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+export const ProductContext = React.createContext({
+    products: []
+});
+
+const initialProducts = [
+    {
+        id: 'p1',
+        title: 'Red Scarf',
+        description: 'A pretty red scarf.',
+        isFavorite: false
+    },
+    {
+        id: 'p2',
+        title: 'Blue T-Shirt',
+        description: 'A pretty blue t-shirt.',
+        isFavorite: false
+    },
+    {
+        id: 'p3',
+        title: 'Green Trousers',
+        description: 'A pair of lightly green trousers.',
+        isFavorite: false
+    },
+    {
+        id: 'p4',
+        title: 'Orange Hat',
+        description: 'Street style! An orange hat.',
+        isFavorite: false
+    }
+];
+
+const ProductContextProvider = (props) => {
+    const [productList, setProductList] = useState(initialProducts);
+
+    return (
+        <ProductContext.Provider value={{products: productList}}>
+            {props.children}
+        </ProductContext.Provider>
+    )
+}
+
+export default ProductContextProvider;
